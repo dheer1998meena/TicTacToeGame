6b7a3ab7f0dd1b165eb17817f2a800a3e523f837
dheer meena <dheer1998meena@gmail.com>
2020-10-11 15:04:11 +0530

    [Dheer] Add. Tic Tac Toe Game

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tic Tac Toe Game");
+            TicTacToe tic = new TicTacToe();
+            // To create a new board 
+          char [] board = tic.CreateBoard();
+        }
+    }
+}
---
+++ TicTacToe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+    class TicTacToe
+    {
+        char player = ' ';
+        char computer = ' ';
+
+        // To create a new board
+        public char [] CreateBoard()
+        {
+            char[] board = new char[10];
+            for (int i=0; i<board.Length; i++)
+            {
+                board[i] = ' ';
+            }
+            return board;
+        }
+    }
+}
